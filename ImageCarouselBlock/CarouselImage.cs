4be5395445d35fb91431c65711b288f38a84b31c
xayidec <34655205+xayidec@users.noreply.github.com>
2024-05-03 10:34:09 -0400

    Delete ImageCarouselBlock directory

--- ImageCarouselBlock/CarouselImage.cs
+++
@@ -1,9 +0,0 @@
-namespace NMIC02_DC.Features.Blocks.ImageCarouselBlock ;
-
-public class CarouselImage
-{
-    public string ImageAltText { get; set; } = "";
-    public string ImageCaption { get; set; } = "";
-    public string ImageUrl { get; set; } = "";
-    public string ImageID { get; set; } = "";
-}
--- ImageCarouselBlock/ImageCarouselConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NMIC02_DC.Features.Blocks.ImageCarouselBlock;
-
-public static class ImageCarouselConstants
-{
-    public const string CarouselId = "carousel-id";
-    public const string CarouselModalId = "carousel-modal";
-    public const string CarouselModalImageId = "carousel-modal-image";
-}
