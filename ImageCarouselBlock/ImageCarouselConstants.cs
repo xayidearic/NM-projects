66e84f80f41341896a47aa6d2e72e79715944ee0
xayidec <34655205+xayidec@users.noreply.github.com>
2024-05-03 09:42:49 -0400

    Add image carousel mvc files

---
+++ ImageCarouselBlock/ImageCarouselConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace NMIC02_DC.Features.Blocks.ImageCarouselBlock;
+
+public static class ImageCarouselConstants
+{
+    public const string CarouselId = "carousel-id";
+    public const string CarouselModalId = "carousel-modal";
+    public const string CarouselModalImageId = "carousel-modal-image";
+}
